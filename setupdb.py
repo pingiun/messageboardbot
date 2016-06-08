3500d781734a6f04a50f885d8438026bb4903e6d
Jelle Besseling <jellebesseling@gmail.com>
2016-06-08 21:40:19 +0200

    Added autoincrement to setupdb
    Previously forgot to auto increment IDs

--- setupdb.py
+++ setupdb.py
@@ -10,17 +10,16 @@ if __name__ == "__main__":
 	conn = sqlite3.connect(sys.argv[1])
 	c = conn.cursor()
 	c.execute("""CREATE TABLE "Channels" (
-	`Channel_ID`	INTEGER NOT NULL UNIQUE,
+	`Channel_ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`ChannelName`	TEXT NOT NULL,
 	`ChannelURL`	TEXT NOT NULL UNIQUE
 )""")
 	c.execute("""CREATE TABLE "Posts_per_Channel" (
-	`Post_ID`	INTEGER NOT NULL UNIQUE,
+	`Post_ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`Replyto_ID`	INTEGER,
 	`Channel_ID`	INTEGER NOT NULL,
 	`ContentType`	TEXT NOT NULL,
-	`Message_ID`	INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY(Post_ID)
+	`Message_ID`	INTEGER NOT NULL UNIQUE
 )""")
 	conn.commit()
 	conn.exit()
